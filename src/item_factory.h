931c1674293ea7a7042d4630053268ee61958a39
KorGgenT <curtis.r.merrill@gmail.com>
2019-01-15 19:20:17 -0500

    stash changes

--- src/item_factory.h
+++ src/item_factory.h
@@ -156,6 +156,7 @@ class Item_factory
         void load_book( JsonObject &jo, const std::string &src );
         void load_comestible( JsonObject &jo, const std::string &src );
         void load_container( JsonObject &jo, const std::string &src );
+        void load_pockets(JsonObject &jo, const std::string &src);
         void load_engine( JsonObject &jo, const std::string &src );
         void load_wheel( JsonObject &jo, const std::string &src );
         void load_fuel( JsonObject &jo, const std::string &src );
