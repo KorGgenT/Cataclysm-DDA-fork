96086e7ac362d2c1c177b93cd9c02f22a567f011
KorGgenT <curtis.r.merrill@gmail.com>
2019-01-19 12:49:59 -0500

    Update forage_tests.cpp

--- tests/forage_tests.cpp
+++ tests/forage_tests.cpp
@@ -15,20 +15,56 @@ std::pair<int, int> forage_calories_and_items( player &p, items_location loc )
             calories = item->type->comestible->get_calories();
         }
         calories_items = std::make_pair( calories_items.first + calories,
-                                         calories_items.second + item->charges );
+                                         calories_items.second + 1 );
     }
     return calories_items;
 }
 
-TEST_CASE( "forage_spring" )
+std::pair<int, int> hundred_bushes_hundred( items_location loc, int survival = 0,
+        int perception = 8 )
 {
     player &dummy = g->u;
     player *dummy_ptr = &dummy;
     std::pair<int, int> calories_items = std::make_pair( 0, 0 );
-    for( int i = 0; i < 100; i++ ) {
-        const auto new_pair = forage_calories_and_items( *dummy_ptr, "forage_spring" );
-        calories_items = std::make_pair( new_pair.first + calories_items.first,
-                                         new_pair.second + calories_items.second );
+    std::pair<int, int> calories_items_total = std::make_pair( 0, 0 );
+    dummy_ptr->set_skill_level( skill_id( "survival" ), survival );
+    dummy.per_cur = perception;
+    for( int j = 0; j < 100; j++ ) {
+        for( int i = 0; i < 100; i++ ) {
+            const auto new_pair = forage_calories_and_items( *dummy_ptr, "forage_spring" );
+            calories_items = std::make_pair( new_pair.first + calories_items.first,
+                                             new_pair.second + calories_items.second );
+        }
+        calories_items_total.first += calories_items.first;
+        calories_items_total.second += calories_items.second;
+        calories_items.first = 0;
+        calories_items.second = 0;
     }
-    printf("calories: %i, number of items: %i", calories_items.first, calories_items.second);
+    return calories_items_total;
+}
+
+TEST_CASE( "forage_spring" )
+{
+    std::pair<int, int> res;
+    printf( "\n" );
+    for( int surv = MAX_SKILL; surv >= 0 ; surv-- ) {
+        res = hundred_bushes_hundred( "forage_spring", surv );
+        printf( "\n100 bushes, 100 runs. Perception 8, Spring.\nSurvival: %i, Total Calories: %i, Total Successes: %i\n",
+                surv, res.first, res.second );
+    }
+    printf("\n");
+    res = hundred_bushes_hundred("forage_spring", 2);
+    printf("\n100 bushes, 100 runs. Perception 8, Spring.\nSurvival: %i, Total Calories: %i, Total Successes: %i\n",
+        2, res.first, res.second);
+    printf("\n");
+    res = hundred_bushes_hundred("forage_spring", 6);
+    printf("\n100 bushes, 100 runs. Perception 8, Spring.\nSurvival: %i, Total Calories: %i, Total Successes: %i\n",
+        6, res.first, res.second);
+}
+
+TEST_CASE("forage_survival9") {
+    printf("\n");
+    const std::pair<int, int> res = hundred_bushes_hundred("forage_spring", 9);
+    printf("\n100 bushes, 100 runs. Perception 8, Spring.\nSurvival: %i, Total Calories: %i, Total Successes: %i\n",
+        9, res.first, res.second);
 }
