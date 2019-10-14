3da6476da82e857d2b4641aac6315f197992acc4
KorGgenT <curtis.r.merrill@gmail.com>
2019-10-14 19:37:42 -0400

    change scope of crossed_threshold()

--- src/player.cpp
+++ src/player.cpp
@@ -2006,7 +2006,7 @@ bool player::has_same_type_trait( const trait_id &flag ) const
     return false;
 }
 
-bool player::crossed_threshold() const
+bool Character::crossed_threshold() const
 {
     for( auto &mut : my_mutations ) {
         if( mut.first->threshold ) {
